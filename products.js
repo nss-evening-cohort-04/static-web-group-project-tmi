3ed4ca49a9c7552962810eb3dab942cfca625c93
Bryan Hankins <bryanhankins@me.com>
2016-08-07 16:36:00 -0500

    Products Final Update

--- products.js
+++ products.js
@@ -1,21 +1,20 @@
 var products = document.getElementById("products");
 var tripSection = [
-{name:"Name", description:"Lorem lean startup ipsum product market fit customer development acquihire technical cofounder. User engagement A/B testing shrink a market venture capital pitch deck. Social bookmarking group buying crowded market pivot onboarding freemium prototype ping pong. Early stage disruptive ecosystem community outreach dynamic location based strategic investor.", image:"img/vacation1.jpg", price:"$99"},
-{name:"Name", description:"Lorem lean startup ipsum product market fit customer development acquihire technical cofounder. User engagement A/B testing shrink a market venture capital pitch deck. Social bookmarking group buying crowded market pivot onboarding freemium prototype ping pong. Early stage disruptive ecosystem community outreach dynamic location based strategic investor.", image:"img/vacation2.jpg", price:"$100"},
-{name:"Name", description:"Lorem lean startup ipsum product market fit customer development acquihire technical cofounder. User engagement A/B testing shrink a market venture capital pitch deck. Social bookmarking group buying crowded market pivot onboarding freemium prototype ping pong. Early stage disruptive ecosystem community outreach dynamic location based strategic investor.", image:"img/vacation3.jpg", price:"$200"},
-{name:"Name", description:"Lorem lean startup ipsum product market fit customer development acquihire technical cofounder. User engagement A/B testing shrink a market venture capital pitch deck. Social bookmarking group buying crowded market pivot onboarding freemium prototype ping pong. Early stage disruptive ecosystem community outreach dynamic location based strategic investor.", image:"img/vacation4.jpg", price:"$300"},
-{name:"Name", description:"Lorem lean startup ipsum product market fit customer development acquihire technical cofounder. User engagement A/B testing shrink a market venture capital pitch deck. Social bookmarking group buying crowded market pivot onboarding freemium prototype ping pong. Early stage disruptive ecosystem community outreach dynamic location based strategic investor.", image:"img/vacation4.jpg", price:"$300"},
-{name:"Name", description:"Lorem lean startup ipsum product market fit customer development acquihire technical cofounder. User engagement A/B testing shrink a market venture capital pitch deck. Social bookmarking group buying crowded market pivot onboarding freemium prototype ping pong. Early stage disruptive ecosystem community outreach dynamic location based strategic investor.", image:"img/vacation4.jpg", price:"$300"},
-{name:"Name", description:"Lorem lean startup ipsum product market fit customer development acquihire technical cofounder. User engagement A/B testing shrink a market venture capital pitch deck. Social bookmarking group buying crowded market pivot onboarding freemium prototype ping pong. Early stage disruptive ecosystem community outreach dynamic location based strategic investor.", image:"img/vacation4.jpg", price:"$300"},
-{name:"Name", description:"Lorem lean startup ipsum product market fit customer development acquihire technical cofounder. User engagement A/B testing shrink a market venture capital pitch deck. Social bookmarking group buying crowded market pivot onboarding freemium prototype ping pong. Early stage disruptive ecosystem community outreach dynamic location based strategic investor.", image:"img/vacation4.jpg", price:"$300"},
-{name:"Name", description:"Lorem lean startup ipsum product market fit customer development acquihire technical cofounder. User engagement A/B testing shrink a market venture capital pitch deck. Social bookmarking group buying crowded market pivot onboarding freemium prototype ping pong. Early stage disruptive ecosystem community outreach dynamic location based strategic investor.", image:"img/vacation4.jpg", price:"$300"}
+{price:"$100", description:"This package is for the real adventurous type that doesn't really plan well. Comes complete with the Family Truckster. Dirt jump not included.", image:"img/vacation1.jpg"},
+{price:"$200", description:"This package is for those of you that have a family member that you'd like to take out to the middle of nowhere and leave for dead. Staple gun included. ", image:"img/vacation2.jpg"},
+{price:"$300", description:"This is for the family that does everything together. Whatever that may be. To be honest you should probably look elsewhere to plan  trip", image:"img/vacation3.jpg"},
+{price:"$400", description:"This package is basically for the boring peeps", image:"img/vacation4.jpg"},
+{price:"$400", description:"This package is basically for the boring peeps", image:"img/vacation4.jpg"},
+{price:"$400", description:"This package is basically for the boring peeps", image:"img/vacation4.jpg"},{price:"$400", description:"This package is basically for the boring peeps", image:"img/vacation4.jpg"},
+{price:"$400", description:"This package is basically for the boring peeps", image:"img/vacation4.jpg"},
+{price:"$400", description:"This package is basically for the boring peeps", image:"img/vacation4.jpg"}
 ];
 
 
 
 for (var i = 0; i < tripSection.length; i++) {
   var destinationCard = "";
-  destinationCard += "<div class='destinationCard'><img class='cardImg' src='"+tripSection[i].image+"'><p>"+(tripSection[i].name)+"</p><p>"+(tripSection[i].description)+"</p><p>"+(tripSection[i].price)+"</p></div>";
+  destinationCard += "<div class='destinationCard'><img class='cardImg' src='"+tripSection[i].image+"'><p>"+(tripSection[i].price)+"</p><p>"+(tripSection[i].description)+"</p><p>"+"</p></div>";
   products.innerHTML += destinationCard
 
   console.log()
